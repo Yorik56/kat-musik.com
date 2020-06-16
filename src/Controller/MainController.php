ec06c7e110e8f7341bbffdfe927ffc8fbdee95b6
moreau <yorik-ba-56@hotmail.Fr>
2020-06-16 13:07:50 +0200

    16/06/2020 - First Commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+
+// use Doctrine\Common\Persistence\ObjectManager;
+// use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+/**
+ * @Route("/")
+ */
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index" )
+     * @return Response
+     * Ce controller permet d'afficher la page d'accueil
+     */
+    public function index(): Response
+    {
+       
+        return $this->render('index/index.html.twig');
+
+    }
+
+    /**
+     * @Route("news/", name="news")
+     * @return Response
+     */
+    public function news(): Response
+    {
+       
+        return $this->render('index/news.html.twig');
+        
+
+    }
+
+}
