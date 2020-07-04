cd1c29d3fc972b6853b6123b70c6a78968a0dbe9
moreau <yorik-ba-56@hotmail.Fr>
2020-07-04 12:06:28 +0200

    commit - 04/07/2020

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,13 +20,27 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="index" )
      * @return Response
+     * @param \Swift_Mailer $mailer
      * Ce controller permet d'afficher la page d'accueil
      */
     public function index(): Response
     {
-
+        // $name = "test";
+        // $message = (new \Swift_Message('Hello Email'))
+        // ->setFrom('yorikmoreau@gmail.com')
+        // ->setTo('yorikmoreau@gmail.com')
+        // ->setBody(
+        //     $this->renderView(
+        //         // templates/emails/registration.html.twig
+        //         'emails/registration.html.twig',
+        //         ['name' => $name]
+        //     ),
+        //     'text/html'
+        // );
+        // $mailer->send($message);
         return $this->render('index/index.html.twig');
 
     }
 
+    
 }
